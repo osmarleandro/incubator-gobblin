68914558dc95e5cb3a79e55abd0799792914d538
Osmar Leandro <osmarleandrods@gmail.com>
2020-12-17 23:20:06 -0300

    gobblin-runtime,org.apache.gobblin.util,SchedulerUtils,loadGenericJobConfig#QProperties;QPath;QPath;QJobSpecResolver,ExtractMethod,OK,false

--- gobblin-runtime/src/main/java/org/apache/gobblin/util/SchedulerUtils.java
+++ gobblin-runtime/src/main/java/org/apache/gobblin/util/SchedulerUtils.java
@@ -134,10 +134,15 @@ public class SchedulerUtils {
     PullFileLoader loader = new PullFileLoader(jobConfigPathDir, jobConfigPathDir.getFileSystem(new Configuration()),
         getJobConfigurationFileExtensions(sysProps), PullFileLoader.DEFAULT_HOCON_PULL_FILE_EXTENSIONS);
 
-    Config sysConfig = ConfigUtils.propertiesToConfig(sysProps);
-    Config config = loader.loadPullFile(jobConfigPath, sysConfig, true);
+    Config config = extracted(sysProps, jobConfigPath, loader);
     return resolveTemplate(ConfigUtils.configToProperties(config), resolver);
   }
+
+private static Config extracted(Properties sysProps, Path jobConfigPath, PullFileLoader loader) throws IOException {
+	Config sysConfig = ConfigUtils.propertiesToConfig(sysProps);
+    Config config = loader.loadPullFile(jobConfigPath, sysConfig, true);
+	return config;
+}
 
   /**
    * Add {@link PathAlterationObserverScheduler}s for the given
