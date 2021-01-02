c182f5c1c4eed41963f35a083e101103c5f415b2
Osmar Leandro <osmarleandrods@gmail.com>
2021-01-02 05:35:51 -0300

    gobblin-audit,org.apache.gobblin.writer,FsWriterMetricsTest,NA,ExtractInterface,OK

---
+++ gobblin-api/src/test/java/org/apache/gobblin/writer/IFsWriterMetricsTest.java
@@ -0,0 +1,11 @@
+package org.apache.gobblin.writer;
+
+import java.io.IOException;
+
+import org.testng.annotations.Test;
+
+public interface IFsWriterMetricsTest {
+
+	void testSerialization() throws IOException;
+
+}
